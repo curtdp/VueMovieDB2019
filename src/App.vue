5db49eb71a2af15078847cab4682e23f69892aa5
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-04-14 23:04:41 +0300

    Изменил стили корневого компонента

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="mx-8">
+    <div id="nav" class="text-center">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -10,11 +10,8 @@
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 #nav {
   padding: 30px;
