50758365244183b33d17ae0f5ca1f71dafed1861
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-04-22 12:33:58 +0300

    Убрал глобальную регистрацию пагинатора

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/main.css";
-import { Pagination } from "vue-pagination-2";
+// import { Pagination } from "vue-pagination-2";
 
-Vue.component("pagination", Pagination);
+// Vue.component("pagination", Pagination);
 
 Vue.config.productionTip = false;
 
