d0457b5867e4d21371fa57bd5d1d20ad9521e3b3
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-04-22 11:01:23 +0300

    Глобальная регистрация vue-pagination (пока не работает) Не коммиттье нерабочий код в мастер на рабочем проекте!!!!!!!!!!!!!!!!!!!!!!!!!!!!! :)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/main.css";
+import { Pagination } from "vue-pagination-2";
+
+Vue.component("pagination", Pagination);
 
 Vue.config.productionTip = false;
 
