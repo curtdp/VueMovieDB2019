4e1a70f3b0b0af2ae690d97b5bb9c3757d6d21e5
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-04-24 20:19:42 +0300

    added i18n plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/main.css";
+import i18n from './i18n'
 // import { Pagination } from "vue-pagination-2";
 
 // Vue.component("pagination", Pagination);
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'uk',
+      fallbackLocale: 'ru',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
+}
